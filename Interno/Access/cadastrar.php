572032924f14d2d4bca04a5457eac7d3697bfc72
RenanJ01 <jonatasrenan02@gmail.com>
2023-05-04 11:15:36 -0300

    up

--- Interno/Access/cadastrar.php
+++ Interno/Access/cadastrar.php
@@ -20,6 +20,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="cadastrar.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" />
     <title>Cadastrar</title>
 </head>
 
@@ -32,48 +33,47 @@
                         <span class="login-form-logo"> <i class="">Imagem</i> 
                             <img src="../../Imagens/icone.ico" alt="ElGames">
                         </span>
-                    <br />
-                    <span class="login-form-title">
-                        Entrar
-                    </span>
-                    <br>
-                    <div class="wrap-input validate-input" data-validate="Entre username">
-                        <input class="input" type="text" id="fuser" name="fuser" placeholder="Nome do Usuario" required>
+                     -->
+
+                    <span class="login-form-logo">
+                        <!-- <i class="">Imagem</i>  -->
+                        <img src="../../Imagens/icone.ico" alt="ElGames">
+                    </span><br />
+
+                    <span class="login-form-title">Cadastrar</span><br />
+
+                    <div class="wrap-input validate-input" data-validate="Entre Nome">
+                        <input type="text" id="fname" name="fname" placeholder="Nome" class="input" required>
+                        <span class="focus-input" data-placeholder="&#xf2c3;"></span>
+                    </div>
+
+                    <div class="wrap-input validate-input" data-validate="Entre Username">
+                        <input type="text" id="fuser" name="fuser" placeholder="Nome do Usuario" class="input" required>
                         <span class="focus-input" data-placeholder="&#xf207;"></span>
                     </div>
-                    <div class="wrap-input validate-input" data-validate="Entre password">
-                        <input class="input" type="password" id="fpass" name="fpass" placeholder="Senha" required>
+
+                    <div class="wrap-input validate-input" data-validate="Entre Senha">
+                        <input type="password" id="fpass" name="fpass" placeholder="Senha" class="input" required>
                         <span class="focus-input" data-placeholder="&#xf191;"></span>
                     </div>
-                    <br />
+
+                    <div class="wrap-input validate-input" data-validate="Entre Genero">
+                        <span class="focus-input" data-placeholder="&#xf211;"><label class="lbl_form" for="fgenero">Gênero:</label></span><br>
+                        <input type="radio" id="fgenero" name="fgenero" checked="true" value="M"><label for="mas">Masculino</label><br>
+                        <input type="radio" id="fgenero" name="fgenero" value="F"><label for="fem">Feminino</label><br>
+                        <input type="radio" id="fgenero" name="fgenero" value="O"><label for="out">Outro</label>
+                    </div>
+
+                    <div class="wrap-input validate-input" data-validate="Entre idade">
+                        <input type="number" id="fidade" name="fidade" min="1" max="127" placeholder="Idade" class="input" required />
+                        <span class="focus-input" data-placeholder="&#xf332;"></span>
+                    </div>
+
                     <div class="ctn-login-form-btn">
-                        <button class="login-form-btn">
-                            Login
-                        </button>
-                    </div> -->
-                    <span class="login-form-title">Cadastrar</span><br>
-
-                    <label for="fname">Nome:</label><br>
-                    <input type="text" id="fname" name="fname" required><br><br>
-
-                    <label for="fuser">Username:</label><br>
-                    <input type="text" id="fuser" name="fuser" required><br><br>
-
-                    <label for="fpass">Senha:</label><br>
-                    <input type="password" id="fpass" name="fpass" required><br><br>
-
-                    <label for="fgenero">Genero:</label><br>
-                    <input type="radio" id="fgenero" name="fgenero" checked="true" value="M"><label for="mas">Masculino</label><br>
-                    <input type="radio" id="fgenero" name="fgenero" value="F"><label for="fem">Feminino</label><br>
-                    <input type="radio" id="fgenero" name="fgenero" value="O"><label for="out">Outro</label><br><br>
-
-                    <label for="fidade">Idade:</label><br>
-                    <input type="number" id="fidade" name="fidade" value="1" min="1" max="127" /><br><br>
-                    <div class="ctn-login-form-btn">
-                        <button class="login-form-btn" type="submit">Cadastrar</button><br/>
+                        <button class="login-form-btn" type="submit">Cadastrar</button><br />
                         <button class="login-form-btn" type="reset">Redefinir</button>
                     </div>
-                    <br/>
+                    <br />
                 </form>
             </div>
         </div>
