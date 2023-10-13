bbc5433ba9c65bcca4fbc2b7016f8ec0027df78c
RenanJ01 <jonatasrenan02@gmail.com>
2023-10-12 21:40:54 -0300

    up

--- Desenvolvimento/desen.php
+++ Desenvolvimento/desen.php
@@ -17,13 +17,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="stylesheet" href="desen.css">
-    <script src="../cont.js"></script>
+    
     <link type="image/ico" rel="icon" href="../Imagens/icone.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>El Games</title>
 </head>
 
-<body onload="SendCookies()">
+<body>
 
     <!-- Cabeçalho da Página -->
     <header>
